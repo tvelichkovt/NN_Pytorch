bead92f7ecf0f213e390189454dd71cc1c3f6264
Todor Velichkov <tvelichkovt@gmail.com>
2020-08-19 10:13:52 +0300

    Create NN_Pytorch1.py

---
+++ NN_Pytorch1.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+from torchvision import transforms, datasets
+
+train = datasets.MNIST('', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ]))
+
+test = datasets.MNIST('', train=False, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ]))
